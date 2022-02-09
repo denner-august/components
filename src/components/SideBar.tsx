2ec7c11615cd6f3d2875814e633dc1c423ed5069
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-02-08 21:09:54 -0300

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+interface paramsProps {
+  selectedGenreId: number;
+  setSelectedGenreId: (selectedGenreId: number) => void;
+  genres: GenreResponseProps[];
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+export function SideBar({
+  selectedGenreId,
+  setSelectedGenreId,
+  genres,
+}: paramsProps): JSX.Element {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre: GenreResponseProps) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => setSelectedGenreId(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
